d0dac7f3009f2322bfa57307ab433d97b54e7fda
Matt Singletary <matt.singletary@gmail.com>
2019-03-24 00:15:19 -0400

    Inline Setup
    Page 36 - 39

--- src/test/java/io/github/msingle/CustomerTest.java
+++ src/test/java/io/github/msingle/CustomerTest.java
@@ -3,70 +3,33 @@ package io.github.msingle;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerTest {
 
-    Customer john, steve, pat, david;
-    String johnName = "John",
-            steveName = "Steve",
-            patName = "Pat",
-            davidName = "David";
-    Customer[] customers;
-
-    @Before
-    public void setup() {
-        david = ObjectMother
-                .customerWithNoRentals(
-                        davidName
-                );
-        john = ObjectMother
-                .customerWithOneNewRelease(
-                        johnName
-                );
-        pat = ObjectMother
-                .customerWithOneOfEachRentalType(
-                        patName
-                );
-        steve = ObjectMother
-                .customerWithOneNewReleaseAndOneRegular(
-                        steveName
-                );
-        customers = new Customer[]{david, john, steve, pat};
-    }
-
     @Test
-    public void getName() {
-        assertEquals(davidName, david.getName());
-        assertEquals(johnName, john.getName());
-        assertEquals(steveName, steve.getName());
-        assertEquals(patName, pat.getName());
-    }
-
-    @Test
-    public void davidStatement() {
+    public void noRentalsStatement() {
         assertEquals(
                 "Rental record for David\nAmount " +
                         "owed is 0.0\n" +
                         "You earned 0 frequent " +
                         "renter points",
-                david.statement());
+                ObjectMother.customerWithNoRentals("David").statement());
 
     }
 
     @Test
-    public void johnStatement() {
+    public void oneNewReleaseStatement() {
         assertEquals(
                 "Rental record for John\n\t" +
                         "Godfather 4\t9.0\n" +
                         "Amount owed is 9.0\n" +
                         "You earned 2 frequent renter points",
-                john.statement());
+                ObjectMother.customerWithOneNewRelease("John").statement());
     }
 
     @Test
-    public void patStatement() {
+    public void allRentalTypesStatement() {
         assertEquals(
                 "Rental record for Pat\n\t" +
                         "Godfather 4\t9.0\n" +
@@ -74,35 +37,18 @@ public class CustomerTest {
                         "\tLion King\t1.5\n" +
                         "%sAmount owed is 14.0\n" +
                         "You earned 4 frequent renter points",
-                pat.statement());
+                ObjectMother.customerWithOneOfEachRentalType("Pat").statement());
     }
 
     @Test
-    public void steveStatement() {
+    public void newReleaseAndRegularStatement() {
         assertEquals(
                 "Rental record for Steve\n\t" +
                         "Godfather 4\t9.0\n" +
                         "\tScarface\t3.5\n" +
                         "%sAmount owed is 12.5\n" +
                         "You earned 3 frequent renter points",
-                steve.statement());
-    }
-
-    @Test
-    public void htmlStatement() {
-        for (int i = 0; i < customers.length; i++) {
-            assertEquals(expStatement(
-                    "<h1>Rental record for " +
-                            "<em>%s</em></h1>\n%s" +
-                            "<p>Amount owed is <em>%s</em>" +
-                            "</p>\n<p>You earned <em>%s" +
-                            " frequent renter points</em></p>",
-                    customers[i],
-                    rentalInfo(
-                            "<p>", "</p>",
-                            customers[i].getRentals())),
-                    customers[i].htmlStatement());
-        }
+                ObjectMother.customerWithOneNewReleaseAndOneRegular("Steve").statement());
     }
 
     @Test(expected = IllegalArgumentException.class)
